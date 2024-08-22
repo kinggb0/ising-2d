c777cea6fd7fc1eafae2cdf1fabd82dbdc3f4450
kinggb0 <kingg@wustl.edu>
2024-08-22 10:51:58 -0500

    updated parallel code

--- ising-2d-para.py
+++ ising-2d-para.py
@@ -3,6 +3,8 @@ import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
 import itertools
+from multiprocessing import Pool
+import os
 ################################
 class walker:
 
@@ -64,25 +66,71 @@ class walker:
    def magnetization(self):
       return np.sum(self.config)
 
-#################################################################
-# TODO: parallelize this and check it matches the serial code
-#################################################################
+# run the Monte carlo
+def mcrun(walker,steps,seed):
+
+   np.random.seed(seed)
+
+   etemp = np.zeros(steps)
+   mtemp = np.zeros(steps)
+
+   for i in range(steps):
+
+      walker.move()
+      etemp[i] = walker.energy()/N**2.
+      mtemp[i] = walker.magnetization()/N**2.
+
+   return etemp, mtemp
 
 #################################################################
-gpts = 100 #temp grid
-N = 32 #lattice sites
-steps = 60000 #time steps
+gpts = 30 #temp grid
+N = 16 #lattice sites
+steps = 12500 #time steps/thrd
 wait = 1 #time between checking moves
+thrds = 8
 
-energies = np.zeros(steps)
+temps = np.linspace(1.5,4.5,gpts)
 
-lattice = walker(N,2.)
+results = np.zeros(thrds)
 
-for i in range(steps):
+energies = np.zeros(gpts)
+magnetzn = np.zeros(gpts)
 
-   lattice.move()
-   energies[i] = lattice.energy()/N**2.
+for j in range(gpts):
 
-fig=plt.figure(figsize=(12,8))
-plt.scatter(np.arange(1,steps+1,1),energies)
+   print("Temp = %.2f" % temps[j])
+
+   lattice = walker(N,temps[j])
+
+   pool = Pool(processes=thrds)
+ 
+   results = [pool.apply_async(mcrun,args=(lattice,steps,os.getpid())) for n in range(thrds)]
+
+   for result in results:
+
+      output = result.get()
+
+      etemp, mtemp = output[0],output[1]
+
+      energies[j] += np.mean(etemp[6750:])
+      magnetzn[j] += np.abs(np.mean(mtemp[6750:]))
+
+energies = energies/thrds
+magnetzn = magnetzn/thrds
+
+fig, (ax1, ax2) = plt.subplots(1,2,figsize=(24,16))
+
+ax1.scatter(temps,energies,s=50,color='darkblue')
+ax2.scatter(temps,magnetzn,s=50,color='orangered')
+
+ax1.set_xlabel(r'$T$ ($k_B^{-1}$)',fontsize=25)
+ax1.set_ylabel(r'$E/N$ ($k_B^{-1})$',fontsize=25)
+ax1.tick_params(axis='both',direction='in',length=4,width=1,labelsize=20)
+
+ax2.set_xlabel(r'$T$ ($k_B^{-1}$)',fontsize=25)
+ax2.set_ylabel(r'$|M|/N$',fontsize=25)
+ax2.tick_params(axis='both',direction='in',length=4,width=1,labelsize=20)
+
+plt.tight_layout()
+plt.savefig('parallel-results.pdf',bbox_inches='tight')
 plt.show()
